7d872f3ba89cfa3fe35a913d3a5671623af47064
jukqaz <jukqaz@gmail.com>
2021-07-22 02:28:15 +0900

    feat: solve warm up challenges

---
+++ interview_preparation_kit/warm_up_challenges/counting_valleys.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+func countingValleys(steps int32, path string) int32 {
+	if int(steps) != len(path) {
+		os.Exit(1)
+	}
+
+	var count int32 = 0
+	level := 0
+	for _, p := range path {
+		if string(p) == "D" {
+			if level == 0 {
+				count++
+			}
+			level--
+		} else {
+			level++
+		}
+	}
+
+	return count
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+	writer := bufio.NewWriterSize(os.Stdout, 16*1024*1024)
+
+	stepsTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	steps := int32(stepsTemp)
+
+	path := readLine(reader)
+
+	result := countingValleys(steps, path)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ interview_preparation_kit/warm_up_challenges/sales_by_match.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+func sockMerchant(n int32, ar []int32) int32 {
+	if int(n) != len(ar) {
+		os.Exit(1)
+	}
+
+	checkMap := make(map[int32]int32)
+
+	var result int32
+	for _, value := range ar {
+		checkMap[value] += 1
+
+		if checkMap[value]%2 == 0 {
+			result++
+		}
+	}
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+	writer := bufio.NewWriterSize(os.Stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var ar []int32
+
+	for i := 0; i < int(n); i++ {
+		arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+		checkError(err)
+		arItem := int32(arItemTemp)
+		ar = append(ar, arItem)
+	}
+
+	result := sockMerchant(n, ar)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
