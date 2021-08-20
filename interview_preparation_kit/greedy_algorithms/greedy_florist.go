3b892c9df255456ba0bd629d75ff7373dba18c45
jukqaz <jukqaz@gmail.com>
2021-08-20 23:52:04 +0900

    feat: solve greedy algorithm

---
+++ interview_preparation_kit/greedy_algorithms/greedy_florist.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// Complete the getMinimumCost function below.
+func getMinimumCost(k int32, c []int32) int32 {
+	result := int32(0)
+
+	sort.Slice(c, func(i, j int) bool {
+		return c[i] > c[j]
+	})
+
+	for i, p := range c {
+		v := int32(i) / k
+		result += p * (v + 1)
+	}
+
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+	writer := bufio.NewWriterSize(os.Stdout, 1024*1024)
+
+	nk := strings.Split(readLine(reader), " ")
+
+	nTemp, err := strconv.ParseInt(nk[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(nk[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	cTemp := strings.Split(readLine(reader), " ")
+
+	var c []int32
+
+	for i := 0; i < int(n); i++ {
+		cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+		checkError(err)
+		cItem := int32(cItemTemp)
+		c = append(c, cItem)
+	}
+
+	minimumCost := getMinimumCost(k, c)
+
+	fmt.Fprintf(writer, "%d\n", minimumCost)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ interview_preparation_kit/greedy_algorithms/max_min.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'maxMin' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY arr
+ */
+
+func maxMin(k int32, arr []int32) int32 {
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] > arr[j]
+	})
+
+	result := int32(1000000000)
+	for i := 0; i <= len(arr)-int(k); i++ {
+		array := arr[i : i+int(k)]
+		diff := array[0] - array[len(array)-1]
+		if diff < result {
+			result = diff
+		}
+	}
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+	writer := bufio.NewWriterSize(os.Stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := maxMin(k, arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
